a2c70f60898666e8861de00e9c903d2146e2df9e
seokhyeonKim91 <89926209+seokhyeonKim91@users.noreply.github.com>
2021-10-18 12:27:10 +0900

    Update project.py

--- project.py
+++ project.py
@@ -1,78 +1,50 @@
 from pico2d import *
 import random
 
-
-class Hero:
-    def __init__(self):
-        self.image = load_image('run_animation.png')
-        self.x, self.y = random.randint(100, 700), 90
-        self.frame = 0
-
-    def update(self): # 소년의 행위 구현.
-        self.x += 5 # 속성 값을 바꿈으로 써, 행위(오른쪽으로 이동)를 구현
-        self.frame = (self.frame + 1 ) % 8
-    def draw(self):
-        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
-
-class Monster:
-    def __init__(self):
-        self.image = load_image('run_animation.png')
-        self.x, self.y = random.randint(100, 700), 90
-        self.frame = 0
-
-    def update(self): # 소년의 행위 구현.
-        self.x += 5 # 속성 값을 바꿈으로 써, 행위(오른쪽으로 이동)를 구현
-        self.frame = (self.frame + 1 ) % 8
-    def draw(self):
-        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
-
-
-class Map:
-    def __init__(self): # 생성자
-        self.image = load_image('map_001.png')
-
-    def draw(self):
-        self.image.draw(400, 30)
-    pass
-
-
 def handle_events():
     global running
+    global dir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
+    pass
 
 # initialization code
 open_canvas()
-
-map =Map() #Grass 라는 클래스로부터, grass 객체를 생성한다.
-hero = Hero()
-team = [ Monster() for i in range(5)]
+map_ground = load_image('dungeon_1.png')
+hero = load_image('link_run.png')
 
 
 running = True
+x = 800 // 2
+frame = 0
+dir = 0
+hide_cursor()
 
 # game main loop code
 
 while running:
-
-    handle_events() # 키 입력을 받아들이는 처리
-
-    #Game logic
-    for monster in team:
-        monster.update() # 소년의 상호작용
-
-    #Game drawing
     clear_canvas()
-    map.draw()
-
-    for monster in team:
-        monster.draw()
+    map_ground.draw()
+    hero.clip_draw()
     update_canvas()
 
+    frame = (frame + 1) % 4
+    handle_events()
+    x += dir * 5
     delay(0.05)
-# finalization code
+close_canvas()
